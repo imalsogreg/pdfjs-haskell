06d7773c87685f2f73181005f3b1cd6b6beef0e3
Greg Hale <imalsogreg@gmail.com>
2020-05-23 21:51:22 -0400

    Hydra

--- reflex-platform.nix
+++ reflex-platform.nix
@@ -5,9 +5,7 @@ let
     rev = "1568e673b3b3c3407d5736e9f33b85b502482a7f";
     sha256 = "05vz3w6fh0gfhihnzgc7mvlyhgqg5cj4qmzq0f6frvi9drn4v460";
   };
-  # reflex-platform-src = builtins.fetchTarball
-  #   {
-  #     url = "https://github.com/reflex-frp/reflex-platform/archive/1568e673b3b3c3407d5736e9f33b85b502482a7f.tar.gz";
-  #   };
 in
-import reflex-platform-src {}
+import reflex-platform-src {
+  nixpkgsFunc = import <nixpkgs>;
+}
