9cd87928bac46c94f50c72b71c9125f54af6f7b0
Greg Hale <imalsogreg@gmail.com>
2020-05-24 20:49:36 -0400

    update

--- release.nix
+++ release.nix
@@ -1,6 +1,8 @@
-{ nixpkgs, reflex-platform }:
+{ nixpkgs ? import <nixpkgs> {}
+, reflex-platform ? import <reflex-platform> {}
+}:
 
-let pdfjs-package = import ./default.nix { inherit reflex-platform };
+let pdfjs-package = import ./default.nix { inherit reflex-platform; };
 in
 {
   pdfjs = pdfjs-package.ghc.pdfjs;
